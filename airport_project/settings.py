66ab4c15f2d0dc8cfe23798c314cb2b168a91074
Студент <student@imes.local>
2025-10-30 10:56:00 +0300

    Initial commit

--- airport_project/settings.py
+++ airport_project/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/agent/dashboard/'
+LOGIN_URL = '/accounts/login/'
+
